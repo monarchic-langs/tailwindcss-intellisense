4a68f03333c13a2f4d6ee8dceb25f122c400cc7b
midischwarz12 <midischwarz@proton.me>
2026-07-29 21:38:12 -0500

    ci: align tailwindcss workflows with fleet policy

--- flake.nix
+++ flake.nix
@@ -7,10 +7,37 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-    formatter.${system} = pkgs.alejandra;
-    packages.${system}.default = pkgs.tailwindcss-language-server;
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [pkgs.tailwindcss-language-server pkgs.pnpm];
+    formatter = {
+      ${system} = pkgs.alejandra;
+    };
+    packages = {
+      ${system}.default = pkgs.tailwindcss-language-server;
+    };
+    checks = {
+      ${system} = {
+        default = pkgs.tailwindcss-language-server;
+
+        flake-format =
+          pkgs.runCommand "tailwindcss-language-server-flake-format-check"
+          {nativeBuildInputs = [pkgs.alejandra];}
+          ''
+            alejandra --check ${./flake.nix}
+            touch $out
+          '';
+
+        package-metadata =
+          pkgs.runCommand "tailwindcss-language-server-package-metadata-check"
+          {}
+          ''
+            test "${pkgs.lib.getName pkgs.tailwindcss-language-server}" = "tailwindcss-language-server"
+            touch $out
+          '';
+      };
+    };
+    devShells = {
+      ${system}.default = pkgs.mkShell {
+        packages = [pkgs.tailwindcss-language-server pkgs.pnpm];
+      };
     };
   };
 }
